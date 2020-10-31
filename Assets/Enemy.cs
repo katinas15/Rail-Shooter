12bd4a95ab6b209fe9e388c87a2f93806f2d66ad
katinas15 <41838906+katinas15@users.noreply.github.com>
2020-11-01 00:57:34 +0200

    as

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,9 +6,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int points = 500;
+    ScoreBoard scoreBoard;
     // Start is called before the first frame update
     void Start()
     {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
@@ -20,6 +23,7 @@ public class Enemy : MonoBehaviour
     }
 
     private void OnParticleCollision(GameObject other) {
+        scoreBoard.ScoreHit(points);
         print("particle collided " + gameObject.name);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
